3c7a23dc199e4959b5c7c06bac33730b8e6b9a0f
gagaruk <red8gams@gmail.com>
2026-07-03 11:54:23 +0300

    page logic

--- mechanical linkage/mechanical_linkage.cpp
+++ mechanical linkage/mechanical_linkage.cpp
@@ -15,7 +15,7 @@ struct page{
   int count = 0;
   page_slot slots[capacity];
   page* prev = nullptr;
-  page_slot* first_free;
+  page_slot* top_free = nullptr;
 };
 
 struct indirectionTable{
@@ -36,16 +36,22 @@ template <typename T>
 void resizeArray(T*& array_ptr, int& current_capacity, double resize_factor);
 
 void create_point(int x, int y);  
-page_slot* allocate_point(int x, int y);
 
-void push_to_gap(page_slot** table_slot);
-page_slot** pop_from_gap();
+page_slot* allocate_point(int x, int y);
+void deallocate_point(page_slot*);
 
 void add_to_table(page_slot* new_point);
 void delete_from_table(int index);
 
+void push_to_gap(page_slot** table_slot);
+page_slot** pop_from_gap();
+
+
+
 indirectionTable pointsTable;
 table_gap_slots gaps;
+
+page* pageStack = new page{};
 
 
 int main(){
@@ -85,10 +91,16 @@ int main(){
 
       break;
     }
-    case 2:
+    case 2:{
+      std::string index;
       std::cout << "Remove point operation selected.\n\n";
-      // Remove po
+
+      std::cout << "Select point(index): ";
+      std::getline(std::cin, index);
+      
+      delete_point(std::stoi(index));
       break;
+    }
     case 3:
       std::cout << "Display points operation selected.\n\n";
       // Display points logic here
@@ -118,13 +130,79 @@ void resize_array(T*& array_ptr, int& current_capacity, double resize_factor){
 }
 
 void create_point(int x, int y){
-  page_slot* slot;
-  return slot;
+  page_slot*  slot = allocate_point(x, y);
+  add_point_to_table(slot);
 }
-
-page_slot* allocate_point_to_page(int x, int y){
+void delete_point(int index){
+  page_slot** table_addr = pointsTable.head+index;
+  deallocate_point(*table_addr);
+  *table_addr = nullptr;
+  push_to_gap(table_addr);
   
 }
+
+page_slot* allocate_point(int x, int y){
+  page_slot* allocation_addr;
+  page* currentPage;
+  if(pageStack->count = 0 && pageStack->prev->count < pageStack->prev->capacity){
+    currentPage = pageStack->prev;
+  }else{
+    currentPage = pageStack;
+  }
+
+  if(pageStack->top_free != nullptr){
+      allocation_addr = pageStack->top_free;
+      pageStack->top_free = pageStack->top_free->next_free;
+  }
+  else{
+    if(currentPage->count >= currentPage->capacity){
+      page* new_page = new page{};
+      new_page->prev = pageStack;
+      new_page->top_free = pageStack->top_free;
+      pageStack = new_page;
+    }
+
+    allocation_addr = currentPage->slots + currentPage->count; 
+  }
+
+  currentPage->count++;
+
+  allocation_addr->data.x = x;
+  allocation_addr->data.y = y;
+
+  return allocation_addr;
+}
+
+void deallocate_point(page_slot* point){
+  point->next_free = pageStack->top_free;
+  pageStack->top_free = point;
+
+  page* pointPage;
+  page* currentPage = pageStack;
+  while(currentPage->prev != nullptr){
+    if(point>currentPage->slots && point<(currentPage->slots+ currentPage->capacity)){
+      pointPage = currentPage;
+      break;
+    }
+  }
+  currentPage->count--;
+  if(currentPage->count==0 && currentPage!=pageStack){
+    delete currentPage;
+  }else if(currentPage->count==0 && currentPage==pageStack && currentPage->prev->count == 0){
+    delete currentPage;
+  }
+}
+
+void add_point_to_table(page_slot* new_point){
+  if(gaps.head != gaps.tail){
+    page_slot** table_addr = pop_from_gap();
+    *table_addr = new_point;
+  }else{
+    *pointsTable.tail = new_point;
+    pointsTable.tail++;
+  }
+}
+
 
 void push_to_gap(page_slot** table_slot){
       if(gaps.head - gaps.tail >= gaps.capacity){
@@ -142,21 +220,5 @@ page_slot** pop_from_gap(){
       }
       gaps.tail--;
       return *gaps.tail;
-}
-
-void add_point_to_table(page_slot* new_point){
-  if(gaps.head != gaps.tail){
-    page_slot** table_addr = pop_from_gap();
-    *table_addr = new_point;
-  }else{
-    *pointsTable.tail = new_point;
-    pointsTable.tail++;
-  }
-}
-
-void delete_point(int index){
-     page_slot** table_addr = pointsTable.head+index;
-      *table_addr = nullptr;
-      push_to_gap(table_addr);
 }
 
